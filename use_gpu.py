9547fb78ce80d121fb28bf8d8f3f334409b698cf
advil64 <advil64@gmail.com>
2024-04-21 23:09:40 -0400

    92% accuracy with the full dataset, LET'S GO

--- use_gpu.py
+++ use_gpu.py
@@ -1,5 +1,5 @@
 from dataloaders.dataset_class_bert import TextDataset
 from torch.utils.data import DataLoader
 
-data = TextDataset('/common/home/ac1771/Desktop/prompt-reconstruction/data', vectorize=True)
+data = TextDataset('/common/home/ac1771/Desktop/prompt-reconstruction/data/diff-files-final', vectorize=True)
 train_loader = DataLoader(data, batch_size=32, shuffle=True)
